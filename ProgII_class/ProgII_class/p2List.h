c6ee715b2f972f34ab2ebb49681a689d92d8839b
Pau5erra <pausc1@TR12-S03-21.tr12.cat>
2015-11-16 16:32:31 +0100

    p2list

---
+++ ProgII_class/ProgII_class/p2List.h
@@ -0,0 +1,21 @@
+#ifndef _P2LIST_
+#define _P2LIST_
+
+class p2List{
+private:
+
+
+	//Constructor
+	//Destructor
+	//push_back
+	//size- retorna el numero de nodes
+	//empty- pregunta si esta vuida
+	//clear- Ho destrueix tot
+	//fer al main que es crei una llista de 5000 nodes
+
+
+
+};
+
+
+#endif
